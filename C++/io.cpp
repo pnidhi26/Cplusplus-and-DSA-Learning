e87d3b3d2368a114145a5e6a121310cf2fbf61aa
pnidhi26 <2016185@iiitdmj.ac.in>
2023-09-18 01:05:10 -0700

    setup

---
+++ C++/io.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a number: " << endl;
+    int number;
+    cin >> number;
+    cout<< "Your Number is: " << number << endl;
+}
